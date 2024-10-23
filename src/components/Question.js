e6569a21d377e8cfa234d2f79cf3180cd9a8cd84
MrSumada <ricksamericana.com>
2024-10-23 17:02:16 -0400

    added basic Question component

---
+++ src/components/Question.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useState } from "react";
+
+const Question = () => {
+
+
+    return (
+        <div>
+            <h2>Question</h2>
+        </div>
+    )
+}
+
+export default Question;
